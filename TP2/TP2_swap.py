4b08a3de4a9d57b0ea682354fbaa5c518e621ea9
Guittet <og67guittet@gmail.com>
2022-11-21 16:57:46 +0100

    Exersice 3 TP3

---
+++ TP2/TP2_swap.py
@@ -0,0 +1,24 @@
+a=input("Entrez la premiere  valeur : ")
+b=input("Entrez la deuxieme  valeur : ")
+c=input("Entrez la troisieme valeur : ")
+
+print("Les valeurs entrees sont : a = " + a + ", b = " + b + " et c = " + c)
+print("Permutation: a ==> b, b ==> c, c ==> a")
+"""      *******************************************
+         * Completez le programme a partir d'ici.
+         *******************************************
+"""
+x = a
+y = b
+z = c
+
+b = x
+c = y
+a = z
+
+"""     *******************************************
+         * Ne rien modifier apres cette ligne.
+         *******************************************
+"""
+
+print("Les valeurs permutees sont : a = " + a + ", b = " + b + " et c = " + c)
