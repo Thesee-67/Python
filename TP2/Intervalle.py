0c8ff04c379e7a7d3da0b5e719f4784736a449e5
Guittet <og67guittet@gmail.com>
2022-11-22 14:59:58 +0100

    TP2 exo8

---
+++ TP2/Intervalle.py
@@ -0,0 +1,10 @@
+x = float(input("Entrez un réel:"))
+
+if 2 >= x < 3 :
+    print(x,"appartient à I")
+elif 0 > x <= 1:
+    print(x,"appartient à I")
+elif -10 >= x <= -2:
+    print(x,"appartient à I")
+else:
+    print(x, "n'appartient pas à I")
