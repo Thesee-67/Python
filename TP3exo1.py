79c9b7dae34871f681ae26c52dddadea1f7c889e
Guittet <og67guittet@gmail.com>
2022-11-22 15:42:00 +0100

    TP3exo1a

---
+++ TP3exo1.py
@@ -0,0 +1,6 @@
+n = int(input("Veillez rentré un entiers naturels N:"))
+somme = 0
+
+for i in range (1, n+1):
+    somme=somme+i
+print("La somme des",n,"premier entiers naturels est égale à",somme)
