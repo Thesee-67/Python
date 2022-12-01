f1aae4af7109cd1d7ed539d38676bcf6b004363d
Guittet <og67guittet@gmail.com>
2022-12-01 09:52:10 +0100

    TP3exo1d

--- TP3/TP3exo1d.py
+++ TP3/TP3exo1d.py
@@ -1,7 +1,13 @@
-n = int(input("Rentrez un nombre:"))
+x = int(input("Rentrez un nombre supérieur a 1:"))
+nombre = 0
+somme = 0
 
-while n > 1:
-    
-
-
+while x < 2:
+        x = int(input("Rentrez un nombre supérieur a 1:"))
+for i in range(0, x+1):
+        somme += nombre
+        if somme > x:
+                break
+        nombre += 1
+print("le nombre recherché est ",nombre - 1)
 
