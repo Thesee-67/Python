e28ef81f9188ccaf389474b90a8897a86e1d77fb
Guittet <og67guittet@gmail.com>
2022-12-01 11:56:04 +0100

    TP3 exo Velo

---
+++ TP3/Velo.py
@@ -0,0 +1,12 @@
+d = int(input("Donnez l’heure de début de la location (un entier) :"))
+f = int(input("Donnez l’heure de fin de la location (un entier) :"))
+
+
+while d == f or d < 0 or d > 25 or f
+    if (d == f):
+        print( "Attention ! l’heure de fin est identique à l’heure de début.")
+    elif (d > f):
+        print("Attention ! le début de la location est après la fin ...")
+    elif (d<f):
+        
+
