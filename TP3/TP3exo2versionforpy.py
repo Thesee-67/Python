814681d188b73ef990c5db15dde71ddf3b3b1f0e
Guittet <og67guittet@gmail.com>
2022-12-01 10:29:29 +0100

    TP3exo2versionforpy

--- TP3/TP3exo2versionforpy.py
+++ TP3/TP3exo2versionforpy.py
@@ -1,15 +1,11 @@
-
-
-
 x = int(input("Rentrez un nombre entier positif:"))
 somme = 0
 
 while x < 0:
     x = int(input("Rentrez un nombre entier positif:"))
-while x >= 0:
+for i in range(0, x):
     somme = x-1
-    x = x-1
+    x=x-1
     print(somme)
     if x == 0:
         break
-
