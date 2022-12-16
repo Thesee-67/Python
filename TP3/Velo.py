e4f5e513b26c71ee85808d0ce17d35fc6d523e7e
Guittet <og67guittet@gmail.com>
2022-12-16 08:55:42 +0100

    TP4

--- TP3/Velo.py
+++ TP3/Velo.py
@@ -1,12 +1,23 @@
 d = int(input("Donnez l’heure de début de la location (un entier) :"))
 f = int(input("Donnez l’heure de fin de la location (un entier) :"))
 
+print (d)
+print (f)
 
-while d == f or d < 0 or d > 25 or f
+
+while d == f or d < 0 or d > 25 or f:
     if (d == f):
         print( "Attention ! l’heure de fin est identique à l’heure de début.")
     elif (d > f):
         print("Attention ! le début de la location est après la fin ...")
     elif (d<f):
+        print("Attention ! la fin de la location est après le début ...")
+
+heure = f - d
+if d >= 0 or d < 7 
+    
+
+
         
+
 
---
+++ TP4/exo-1.py
@@ -0,0 +1,5 @@
+n = float(input("Enter any Number  :"));
+
+for i in range(1,11):
+ value = n * i
+ print(n," * ",i," = ",value)
