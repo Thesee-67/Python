eeaea89260f6e4704b3ce0090e07a612e0d313f0
Guittet <og67guittet@gmail.com>
2023-01-04 10:12:40 +0100

    TP5

--- TP5/nomprenom.py.py
+++ TP5/nomprenom.py.py
@@ -1,2 +1,14 @@
-nom = int(input("Quelle est votre Nom ?"))
-prenom = int (input("Quelle est votre Prénom ?"))
+nom = str(input("Quelle est le nom de la première personne ?"))
+prenom = str(input("Quelle est le Prénom de la première personne ?"))
+nom2 = str(input("Quelle est le Nom de la deuxième personne ?"))
+prenom2 = str(input("Quelle est le Prénom de la deuxième persone ?"))
+
+if nom < nom2:
+    print(str.capitalize(prenom),str.upper(nom))  
+    print(str.capitalize(prenom2),str.upper(nom2))
+elif nom > nom2:
+    print(str.capitalize(prenom),str.upper(nom))
+    print(str.capitalize(prenom2),str.upper(nom2))
+elif nom == nom2:
+    print(str.capitalize(prenom),str.upper(nom))
+    print(str.capitalize(prenom2),str.upper(nom2))
