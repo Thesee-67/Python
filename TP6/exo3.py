3639ad171057d60e7e5c04667ae997ca7076ff20
Guittet <og67guittet@gmail.com>
2023-01-19 23:57:47 +0100

    Python

--- TP6/exo3.py
+++ TP6/exo3.py
@@ -9,3 +9,14 @@ print(ajouter_elt())
 
 print(ajouter_elt())
 #on remarque que le 3 apparait 2 fois a la fin de la liste contrairement a avant ou il apparait qu'une seule fois.
+
+def ajouter_carac(ch = "abc", elt = "d"):
+    return ch + elt
+
+print(ajouter_carac())
+
+#le résultat de la fonction ajouter carac est abcd
+
+print(ajouter_carac())
+
+# le résultat de la fonction est toujours le meme.
